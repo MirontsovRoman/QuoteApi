4cbf20ff8dcd4cb573d971ab7bf114c3e264e907
Roman <roma419@mail.ru>
2023-09-22 20:01:38 +0300

    add Flask-* library in pipenv

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from api import app
 from api.handlers import author
 from api.handlers import quote
 from config import Config
+from flask_sqlalchemy import SQLAlchemy
 
 
 if __name__ == '__main__':
