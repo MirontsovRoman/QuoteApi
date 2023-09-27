149893da01b4b072e1aedca3d50f9ed61154c03f
Roman <roma419@mail.ru>
2023-09-27 18:52:50 +0300

    Add Homework

--- ma_examples/author.py
+++ ma_examples/author.py
@@ -1,9 +1,18 @@
 class Author:
-   def __init__(self, id, name, email="def@mail.ru"):
-       self.id = id
-       self.name = name
-       self.email = email
-    #    self.surname = surname
+    def __init__(self, id, name, email="def@mail.ru"):
+        self.id = id
+        self.name = name
+        self.email = email
 
-   def __repr__(self):
-       return f"Author({self.id}): {self.name}|{self.email}"
+    def __repr__(self):
+        return f"Author({self.id}): {self.name}|{self.email}"
+
+
+class Learner:
+    def __init__(self, uid, name, final_test):
+        self.uid = uid
+        self.name = name
+        self.final_test = final_test
+
+    def __repr__(self):
+        return f"Author({self.uid}): {self.name}|{self.final_test}"
--- ma_examples/schema.py
+++ ma_examples/schema.py
@@ -1,7 +1,25 @@
-from marshmallow import Schema, fields
+from marshmallow import Schema, fields, validate
 
 
 class AuthorSchema(Schema):
-   id = fields.Integer()
-   name = fields.Str(required=True, error_messages={'required': {'message': 'name is required', 'code': 400}})
-   email = fields.Email(required=True, error_messages={'required':'email is required'})
+    id = fields.Integer()   # int()
+    name = fields.Str(required=True, error_messages={'required': {
+                      'message': 'name is required', 'code': 400}})
+    email = fields.Email(required=True, error_messages={
+        'required': 'email is required'})
+
+
+class LearnerSchema(Schema):
+    uid = fields.Integer()
+    name = fields.Str(
+        required=True,
+        error_messages={
+            'required': {
+                'message': 'name is requided',
+                'code': 400
+            }
+        },
+        validate=[validate.Length(max=50)]
+    )
+    final_test = fields.Boolean(required=True, error_messages={
+        'required': 'final_test is required'})
