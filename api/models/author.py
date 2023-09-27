c186e4ede48d4ec16ea5cb77e2bab192b9a6cf4e
Roman <roma419@mail.ru>
2023-09-27 21:04:48 +0300

    apply ma schemas

--- api/models/author.py
+++ api/models/author.py
@@ -6,13 +6,8 @@ class AuthorModel(db.Model):
     name = db.Column(db.String(32))
     # Один из вариантов решения для каскадного удаления, связанных с автором цитат.
     quotes = db.relationship('QuoteModel', backref='author', lazy='dynamic', cascade='all, delete')
-    # quotes = db.relationship('QuoteModel', backref='author', lazy='dynamic')
-
+    
     def __init__(self, name):
         self.name = name
 
-    # def to_dict(self):
-    #     return {
-    #         "id": self.id,
-    #         "name": self.name
-    #     }
+ 
