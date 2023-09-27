2eb5c4bd343219653bdedff0f3c2c5dd98d55371
Roman <roma419@mail.ru>
2023-09-27 19:47:24 +0300

    Add pattern matching check

---
+++ api/schemas/author.py
@@ -0,0 +1,11 @@
+from api import ma
+from api.models.author import AuthorModel
+from api.models.quote import QuoteModel
+
+
+class AuthorSchema(ma.SQLAlchemyAutoSchema):
+   class Meta:
+       model = AuthorModel
+
+author_schema = AuthorSchema()
+authors_schema = AuthorSchema(many=True)
